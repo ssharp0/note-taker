e0b52a95fe3f3e958e9149ac947e069010752094
SHARP <ssharp9999@gmail.com>
2021-09-13 09:56:08 -0700

    db setup

---
+++ db/index.js
@@ -0,0 +1,5 @@
+// export notes
+module.exports = {
+ // require the notes js file
+ notes: require('./notes.js')
+}
---
+++ db/notes.js
@@ -0,0 +1,16 @@
+// notes array to store and manage notes in database
+const notes = [
+ {
+  noteTitle: 'This is just some random title to show',
+  noteText: 'This is just a test note',
+  isMarked: false
+ },
+ // {
+ //  noteTitle: 'Second example',
+ //  noteText: 'Second example note text',
+ //  isMarked: false
+ // },
+]
+
+// export notes
+module.exports = notes
