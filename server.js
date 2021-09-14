d0f9efcf9c2eb5f48e3677d1151a58a9a36b4dae
SHARP <ssharp9999@gmail.com>
2021-09-13 20:42:07 -0700

    minor server updates routes

--- server.js
+++ server.js
@@ -1,17 +1,25 @@
-// require express and path to set up routes to build the server
+// require express (and router) and path to set up routes to build the server dependencies
 const express = require('express')
 const { join } = require('path')
+const router = require('express').Router()
 
-// set express to app to use
+// require routes for the notes (API) and html - point server to route files
+const notesRoutes = require('./routes/noteRoutes')
+const htmlRoutes = require('./routes/htmlRoutes')
+
+
+// set express to app for use
 const app = express()
 
-// middleware to serve up front end
+// middleware to serve up front end - sets up data parsing required for API calls
 app.use(express.static(join(__dirname, 'public')))
 app.use(express.urlencoded({extended : true}))
 app.use(express.json())
 
-// require the routes
+// application use routes for notes (API) and html
 app.use(require('./routes'))
+app.use('/', htmlRoutes)
+app.use('/api', notesRoutes)
 
 // server port - set up for port in Heroku or local host 3000
 app.listen(process.env.PORT || 3000)
