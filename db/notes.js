b025007c49f75ca8050aaab35b78024500f1bfec
SHARP <ssharp9999@gmail.com>
2021-09-13 20:42:18 -0700

    minor update

--- db/notes.js
+++ db/notes.js
@@ -1,14 +1,14 @@
 // notes array to store and manage notes in database
 const notes = [
  {
-  noteTitle: 'This is just some random title to show',
-  noteText: 'This is just a test note',
-  isMarked: false
+  noteTitle: 'Example Note Title. (Click me to see body)',
+  noteText: 'Example Note Text - This is the body of the note text. Use the pencil icon to create a new note! Start typing in the note title and note body to show the save button to save the note!',
+  noteId: '0'
  },
  // {
  //  noteTitle: 'Second example',
  //  noteText: 'Second example note text',
- //  isMarked: false
+ //  id: 1
  // },
 ]
 
